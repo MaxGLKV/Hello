17ca163ab05b30fcf680f16e19366d65159347ee
MaxGLKV <maxgolikv@gmail.com>
2023-09-07 14:25:18 +0300

    dDD

--- ex9.py
+++
@@ -1,6 +0,0 @@
-age = input("How old are you? ")
-weight = input("How much do you weight? ")
-height = input("How tall are you? ")
-
-print("Soool, you're %r old, %r heavy and %r tall. " % (age, weight,
-height))
